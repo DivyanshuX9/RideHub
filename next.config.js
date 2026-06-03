2ff23d2690991ee8e2a0f4be3f1c4c37ea0ab5f0
DivyanshuX9 <divyanshu88999@gmail.com>
2026-06-03 15:19:58 +0530

    Next configue update :)

--- next.config.js
+++ next.config.js
@@ -20,16 +20,6 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  ...(!isMobile && {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8000'}/:path*`,
-        },
-      ];
-    },
-  }),
 };
 
 module.exports = nextConfig;
